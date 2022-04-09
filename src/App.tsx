cf6120c206f8e0fa1967fdffd808727dda7146c8
vjvic <victorsulit0@gmail.com>
2022-04-09 17:38:09 +0800

    Add timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 interface Quiz {
   id: number;
@@ -96,6 +96,33 @@ function App() {
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
 
+  const [seconds, setSeconds] = useState(59);
+  const [minutes, setMinutes] = useState(5);
+
+  function updateTime() {
+    if (seconds === 0) {
+      setMinutes((minutes) => minutes - 1);
+      setSeconds(59);
+    } else {
+      setSeconds((seconds) => seconds - 1);
+    }
+  }
+
+  /*eslint-disable */
+  useEffect(() => {
+    const token = setTimeout(updateTime, 1000);
+
+    if (seconds === 0 && minutes === 0) clearTimeout(token);
+
+    if (minutes === 0 && seconds === 0) {
+      setShowScore(true);
+    }
+
+    return function cleanUp() {
+      clearTimeout(token);
+    };
+  });
+
   const totalQuiz = quizItems.length;
 
   const handleClick = (isCorrect: boolean) => {
@@ -105,9 +132,10 @@ function App() {
 
     if (currentQuiz + 1 < totalQuiz) {
       setCurrentQuiz((prevState) => prevState + 1);
-      console.log("hello");
     } else {
       setShowScore(true);
+      setMinutes(0);
+      setSeconds(0);
     }
   };
 
@@ -115,42 +143,49 @@ function App() {
     setCurrentQuiz(0);
     setScore(0);
     setShowScore(false);
+    setMinutes(5);
+    setSeconds(59);
   };
 
   return (
-    <section className="quiz-container">
-      {showScore ? (
-        <div className="result">
-          <div>
-            <p>
-              Score: {score}/{totalQuiz}
-            </p>
-            <button onClick={handleRetry}>Retry</button>
+    <>
+      <div>
+        {minutes}:{seconds}
+      </div>
+      <section className="quiz-container">
+        {showScore ? (
+          <div className="result">
+            <div>
+              <p>
+                Score: {score}/{totalQuiz}
+              </p>
+              <button onClick={handleRetry}>Retry</button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <>
-          <span className="quiz-number">
-            {currentQuiz + 1} of {totalQuiz}
-          </span>
-          <div className="quiz-question">
-            <span className="question-heading">Question:</span>
-            <p className="question-text">{quizItems[currentQuiz].question}</p>
-          </div>
+        ) : (
+          <>
+            <span className="quiz-number">
+              {currentQuiz + 1} of {totalQuiz}
+            </span>
+            <div className="quiz-question">
+              <span className="question-heading">Question:</span>
+              <p className="question-text">{quizItems[currentQuiz].question}</p>
+            </div>
 
-          <div className="answers">
-            {quizItems[currentQuiz].answers.map((answer) => (
-              <button
-                onClick={() => handleClick(answer.isCorrect)}
-                key={answer.id}
-              >
-                {answer.answerText}
-              </button>
-            ))}
-          </div>
-        </>
-      )}
-    </section>
+            <div className="answers">
+              {quizItems[currentQuiz].answers.map((answer) => (
+                <button
+                  onClick={() => handleClick(answer.isCorrect)}
+                  key={answer.id}
+                >
+                  {answer.answerText}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
+      </section>
+    </>
   );
 }
 
